1d23dd758fa1b129bd4099b213f0ce875eddca0d
sunshowers <rain@sunshowers.io>
2022-06-11 05:44:45 +0000

    Deploying to gh-pages from @ nextest-rs/nextest@862cef5029d1a261de57a8502fcd27bcf4d88939 🚀

--- rustdoc/nextest_runner/cargo_config/sidebar-items.js
+++ rustdoc/nextest_runner/cargo_config/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["CargoConfigs","A store for Cargo config files discovered from disk."]]});
+initSidebarItems({"enum":[["TargetTripleSource","The place where a target triple’s configuration was picked up from."]],"struct":[["CargoConfigs","A store for Cargo config files discovered from disk."],["TargetTriple","Represents a target triple that’s being cross-compiled against."]]});
